aa4c53add21046f8a5aa2cd70db91d4614ea1d10
dwairi <qaisdw96@hotmail.com>
2023-06-15 16:44:39 +0000

    d

---
+++ web_scraper/scraper.py
@@ -0,0 +1,39 @@
+import requests
+from  bs4 import BeautifulSoup
+
+
+URL = 'https://en.wikipedia.org/wiki/History_of_Morocco'
+
+
+def get_citations_needed_count(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content,'html.parser')
+    all_paragraphs = soup.find_all('p')
+
+    paragraphs_that_contain_citations = ''
+    for paragraph in all_paragraphs:
+        citations = paragraph.find_all('sup',class_='noprint Inline-Template Template-Fact')
+        if citations :
+            paragraphs_that_contain_citations += paragraph.text
+
+    return paragraphs_that_contain_citations.count('citation needed')
+
+
+
+def get_citations_needed_report(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content,'html.parser')
+    all_paragraphs = soup.find_all('p')
+
+    list_of_paragraphs_that_contain_citations = []
+    for paragraph in all_paragraphs:
+        citations = paragraph.find_all('sup',class_='noprint Inline-Template Template-Fact')
+        if citations :
+            list_of_paragraphs_that_contain_citations.append(paragraph.text)
+
+    return list_of_paragraphs_that_contain_citations
+        
+
+
+print(get_citations_needed_count(URL))
+print(get_citations_needed_report(URL))
